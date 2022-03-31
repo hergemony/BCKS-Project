25f972b90eeae52c2ef0d0dd896be151feee9068
Simon Levey <lev_47@hotmail.com>
2022-03-31 19:56:07 +0800

    currency-converter

--- js/menu.js
+++ js/menu.js
@@ -1,82 +1,42 @@
-/* Control sliding menu on screens smaller than a specified breakpoint */
-(function(menu_button, links, breakpoint) {
-	'use strict';
-	var menulink = document.getElementById(menu_button),
-	    menu = document.getElementById(links);
-		
-	menu.className = 'start';
-	setTimeout(function() {
-		menu.className = 'collapsed';
-	}, 20);
-		
-	menulink.onclick = function() {
-		if (menu.className === 'displayed') {
-			menu.className = 'collapsed';
-		} else {
-			menu.className = 'displayed';
-		}
-		return false;
-	};
-	
-	window.onresize = function() {
-		if (window.innerWidth < breakpoint) {
-			menu.className = 'collapsed';
-		}
-	};	
-})('menulink', 'navlinks', 700);
+const select = document.querySelectorAll(".currency");
+const btn = document.getElementById("btn");
+const num = document.getElementById("num");
+const ans = document.getElementById("ans");
 
-// testing current affairs api
-//https://rapidapi.com/MyAllies/api/breaking-news/
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'myallies-breaking-news-v1.p.rapidapi.com',
-		'X-RapidAPI-Key': '992635b04amsh3ecf4c948377782p1f271bjsn21129e5c436d'
-	}
-};
+fetch("https://api.frankfurter.app/currencies")
+  .then((data) => data.json())
+  .then((data) => {
+    display(data);
+  });
 
-fetch('https://myallies-breaking-news-v1.p.rapidapi.com/GetCompanyDetailsBySymbol?symbol=twtr', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+function display(data) {
+  const entries = Object.entries(data);
+  for (var i = 0; i < entries.length; i++) {
+    select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+    select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+  }
+}
 
+btn.addEventListener("click", () => {
+  let currency1 = select[0].value;
+  let currency2 = select[1].value;
+  let value = num.value;
 
+  if (currency1 != currency2) {
+    convert(currency1, currency2, value);
+  } else {
+    alert("Choose Different Currencies !!!");
+  }
+});
 
-// testing Zippotamus API
-// http://www.zippopotam.us/#where
-<script type="text/javascript">
-var client = new XMLHttpRequest();
-client.open("GET", "http://api.zippopotam.us/us/90210", true);
-client.onreadystatechange = function() {
-	if(client.readyState == 4) {
-		alert(client.responseText);
-	};
-};
-
-client.send();
-</script>
-
-// anger management api
-https://rapidapi.com/blog/how-to-use-an-api-with-javascript/
-
-
-//boring api
-
-let url = "http://www.boredapi.com/api/activity/"
-
-fetch('http://www.boredapi.com/api/activity/')
-.then(response =>{
-    return response.json();
-}).then(data =>{
-    console.log(data);
-})
-
-
-// random useless fact
-https://www.programmableweb.com/api/random-useless-facts-rest-api-v10
-
-https://uselessfacts.jsph.pl/
-
-
-
-
+function convert(currency1, currency2, value) {
+  const host = "api.frankfurter.app";
+  fetch(
+    `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
+  )
+    .then((val) => val.json())
+    .then((val) => {
+      console.log(Object.values(val.rates)[0]);
+      ans.value = Object.values(val.rates)[0];
+    });
+}
